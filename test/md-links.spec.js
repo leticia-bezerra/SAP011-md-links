a65d9f405228a83e2095f86aea7afe8f2b0215a9
Leticia Bezerra <leticia21bezerradearaujo@gmail.com>
2023-12-12 22:34:02 -0300

    finish

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,6 +1,11 @@
 const { mdLinks } = require('../index.js');
 
 describe('mdLinks', () => {
+
+  // it('should return error if invalid path ', () => {
+  //   return  expect(mdLinks('./test.md', false)).rejects.toBeDefined("error") 
+  // });
+
   it('should return extracted links when validate is false', () => {
     return mdLinks('./test/test.md', false).then((links) => {
       expect(links).toStrictEqual([
@@ -13,15 +18,13 @@ describe('mdLinks', () => {
   });
 
   it('should return extracted links when validate is true', () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      status:200
-    }));
+    global.fetch = jest.fn(() => Promise.reject({}));
     return mdLinks('./test/test.md', true).then((links) => {
       expect(links).toStrictEqual([
-        { href: "https://www.youtube.com/watch?v=Lub5qOmY4JQ", text: "recurso", file: "./test/test.md", status:"200", ok: "ok"  },
-        { href: "https://www.youtube.com/watch?v=Lub5qOmY4JQ", text: "recurso ll", file: "./test/test.md",  status:"200", ok: "ok" },
-        { href: "https://www.youtube.com/watch?v=Lub5qOmY4JQ", text: "recurso lll", file: "./test/test.md",  status:"200", ok: "ok" },
-        { href: "https://www.youtube.com/leticiav=Lubkejfrngiuhdio5qOmY4JQ", text: "recurso lV", file: "./test/test.md",  status:"ENOTFOUND", ok: "fail" },
+        { href: "https://www.youtube.com/watch?v=Lub5qOmY4JQ", text: "recurso", file: "./test/test.md", status: "ENOTFOUND", ok: "fail" },
+        { href: "https://www.youtube.com/watch?v=Lub5qOmY4JQ", text: "recurso ll", file: "./test/test.md", status: "ENOTFOUND", ok: "fail" },
+        { href: "https://www.youtube.com/watch?v=Lub5qOmY4JQ", text: "recurso lll", file: "./test/test.md", status: "ENOTFOUND", ok: "fail" },
+        { href: "https://www.youtube.com/leticiav=Lubkejfrngiuhdio5qOmY4JQ", text: "recurso lV", file: "./test/test.md", status: "ENOTFOUND", ok: "fail" },
       ]);
     });
   });
